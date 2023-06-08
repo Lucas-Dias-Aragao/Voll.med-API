5ad25d36f33c7a9966e059585b6ee6d3f9760775
LucasDias <lucasd.aragao@hotmail.com>
2023-06-08 12:27:53 -0300

    adiciona metodos put e delete para medicos e pacientes e cria readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# Voll.med API
+
+### API desenvolvida para a clínica médica Voll Med usando Spring Boot 3
+
+<p>Neste projeto apliquei os conteúdos estudados de Spring Boot para a criação de um CRUD para cadastro de médicos  
+e pacientes.</p>
+
+### Foram utilizados no projeto:
+* Spring Boot 3
+* Java
+* Insomnia
+* Maven
+* JPA/Hibernate
+* MySQL/Flyway
+* Lombok
+
+#### Em andamento:
+
+* Tratamento de erros
+* Autorização/autenticação com JWT
--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -22,12 +22,30 @@ public class Paciente {
     private String email;
     @Embedded
     private Endereco endereco;
+    private Boolean ativo;
 
     public Paciente(DadosCadastroPaciente dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.cpf = dados.cpf();
         this.email = dados.email();
         this.telefone = dados.telefone();
         this.endereco = new Endereco(dados.endereco());
+    }
+
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+        if(dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+            this.nome = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+    public void inativar(){
+        this.ativo = false;
     }
 }
